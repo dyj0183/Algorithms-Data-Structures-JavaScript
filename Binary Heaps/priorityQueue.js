c3f0a9e205f237e9338be013f754017b0940ffaa
Yu-Chun Tung <dyj0183@byui.edu>
2022-04-20 10:55:24 -0600

    finished the logic of bubbling up by comparing the priority against parent node's priority

--- Binary Heaps/priorityQueue.js
+++ Binary Heaps/priorityQueue.js
@@ -6,25 +6,40 @@
 
 // We are gonna write a "Min Binary Heap" because lower number means higher priority, ex: 1 is the highest in the priority
 class PriorityQueue {
-    constructor() {
-        this.values = [];
-    }
+	constructor() {
+		this.values = [];
+	}
 
-    insert(val, priority) {
-        let newNode = new Node(val, priority);
-        this.values.push(newNode);
-        // compare against the parent node by looking into the priority, if it is higher priority, then move up
-        this.bubbleUp();
-    }
+	enqueue(val, priority) {
+		let newNode = new Node(val, priority);
+		this.values.push(newNode);
+		// Compare against the parent node by looking into the priority, if it is higher priority, then move up
+		this.bubbleUp();
+	}
 
-    bubbleUp() {
+	bubbleUp() {
+		let idx = this.values.length;
+		const node = this.values[idx];
 
-    }
+        // Index can't be less than 0
+		while (idx > 0) {
+			// This is the formula to find the parent node index 
+			let parentIdx = Math.floor((idx - 1) / 2);
+            let parentNode = this.values[parentIdx];
+
+            // If the node's priority is greater or equal to parent node's priority, then no need to swap, leave the loop  
+            if (parentNode.priority <= node.priority) break;
+            // Otherwise, swap the node and update the current node's index
+            this.values[parentIdx] = node;
+            this.values[idx] = parentNode;
+            idx = parentIdx;
+		}
+	}
 }
 
 class Node {
-    constructor(val, priority) {
-        this.val = val;
-        this.priority = priority;
-    }
+	constructor(val, priority) {
+		this.val = val;
+		this.priority = priority;
+	}
 }
