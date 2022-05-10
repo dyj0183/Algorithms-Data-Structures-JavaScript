1d2130e4d6c3fce29b7fd4932b3d37fdfc7aada5
Yu-Chun Tung <dyj0183@byui.edu>
2022-05-10 08:40:56 -0600

    updated the codes to pass all test cases for the first duplicate value challenge

--- Coding Challenge Solutions/Arrays/first-duplicate-value.js
+++ Coding Challenge Solutions/Arrays/first-duplicate-value.js
@@ -1,16 +1,34 @@
+/*
+Question:
+Find out the first duplicated number in the array, if no duplicated number, return -1
+
+Sample Input:
+[2, 1, 5, 2, 3, 3, 4]
+
+Sample Output: 
+2 (First duplicated number in the array)
+*/
+
+// My solution
+// Time: O(n), Space: O(1)
 function firstDuplicateValue(array) {
-	let myObject = {};
-	let duplicated;
+	const myObject = {};
+	// Set default to -1 because if there is no integer appears more than once, return -1
+	let duplicatedNum = -1;
 	for (let num of array) {
-		if (!num in myObject) {
+		if (!(num in myObject)) {
 			myObject[num] = 1;
 		} else {
-			duplicated = num;
+			// If the "num" already exists in the object, that means it is duplicated which is the num we are looking for
+			duplicatedNum = num;
 			break;
 		}
 	}
-	return duplicated;
+	return duplicatedNum;
 }
+
+const testArray = [2, 1, 5, 2, 3, 3, 4];
+console.log(firstDuplicateValue(testArray));
 
 // Do not edit the line below.
 exports.firstDuplicateValue = firstDuplicateValue;
