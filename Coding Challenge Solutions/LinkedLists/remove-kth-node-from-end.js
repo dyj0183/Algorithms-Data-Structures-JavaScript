7b02b34659e5a42efd6002319e5adb8873c25941
Yu-Chun Tung <dyj0183@byui.edu>
2022-05-23 22:46:34 -0600

    tried a different solution for this challenge

--- Coding Challenge Solutions/LinkedLists/remove-kth-node-from-end.js
+++ Coding Challenge Solutions/LinkedLists/remove-kth-node-from-end.js
@@ -7,30 +7,52 @@ class LinkedList {
 }
 
 function removeKthNodeFromEnd(head, k) {
-	let linkedListLength = 0;
-	let node = head;
-	while (node !== null) {
-		linkedListLength++;
-		node = node.next;
-	}
-	// edge case, the k doesn't exist
-	if (linkedListLength < k || k < 1) {
-		return;
-	}
+    let counter = 1;
+    let first = head;
+    let second = head;
 
-	let removedPosition = linkedListLength - k + 1;
-	let currentPosition = 1;
-	let removedNode = head;
-	let prevOfRemovedNode = null;
-	while (removedPosition !== currentPosition && node !== null) {
-		currentPosition++;
-		prevOfRemovedNode = removedNode;
-		removedNode = removedNode.next;
-	}
+    while (counter <= k) {
+        second = second.next;
+        counter++;
+    }
 
-	prevOfRemovedNode.next = removedNode.next;
+    if (second === null) {
+        head.value = head.next.value;
+        head.next = head.next.next;
+        return;
+    }
 
-	console.log(linkedListLength);
+    while (second.next !== null) {
+        second = second.next;
+        first = first.next;
+    }
+    
+    first.next = first.next.next;
+
+	// let linkedListLength = 0;
+	// let node = head;
+	// while (node !== null) {
+	// 	linkedListLength++;
+	// 	node = node.next;
+	// }
+	// // edge case, the k doesn't exist
+	// if (linkedListLength < k || k < 1) {
+	// 	return;
+	// }
+
+	// let removedPosition = linkedListLength - k + 1;
+	// let currentPosition = 1;
+	// let removedNode = head;
+	// let prevOfRemovedNode = null;
+	// while (removedPosition !== currentPosition && node !== null) {
+	// 	currentPosition++;
+	// 	prevOfRemovedNode = removedNode;
+	// 	removedNode = removedNode.next;
+	// }
+
+	// prevOfRemovedNode.next = removedNode.next;
+
+	// console.log(linkedListLength);
 }
 
 // Do not edit the lines below.
