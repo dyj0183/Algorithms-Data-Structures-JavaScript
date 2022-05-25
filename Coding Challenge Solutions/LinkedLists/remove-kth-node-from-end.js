eed3d1a1178913400bcb16fdd48f902337ad7707
Yu-Chun Tung <dyj0183@byui.edu>
2022-05-24 21:42:24 -0600

    cleaned up the codes and added comments, completed the coding challenge of remove kth node from end

--- Coding Challenge Solutions/LinkedLists/remove-kth-node-from-end.js
+++ Coding Challenge Solutions/LinkedLists/remove-kth-node-from-end.js
@@ -1,3 +1,16 @@
+/*
+Challenge: 
+remove the kth node from the end of a singly linked list
+
+Sample Input:
+head = 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
+k = 4 
+
+Sample Output: 
+0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 7 -> 8 -> 9
+the 4th node from the end (number "6") is removed
+*/
+
 // This is an input class. Do not edit.
 class LinkedList {
 	constructor(value) {
@@ -6,53 +19,32 @@ class LinkedList {
 	}
 }
 
+// use the technique of having two pointers to traverse through the entire singly linked list and find the right one
 function removeKthNodeFromEnd(head, k) {
     let counter = 1;
     let first = head;
     let second = head;
 
+    // first part, move the second pointer to have the right space (k) between itself and the first pointer (head)
     while (counter <= k) {
         second = second.next;
         counter++;
     }
 
+    // edge case, if the second pointer is null, means already reach the end, so head node is the one we want to remove
     if (second === null) {
         head.value = head.next.value;
         head.next = head.next.next;
         return;
     }
 
+    // otherwise, keep going until the second pointer hits the end (null), then we find the node (first pointer) we would like to remove
     while (second.next !== null) {
         second = second.next;
         first = first.next;
     }
     
     first.next = first.next.next;
-
-	// let linkedListLength = 0;
-	// let node = head;
-	// while (node !== null) {
-	// 	linkedListLength++;
-	// 	node = node.next;
-	// }
-	// // edge case, the k doesn't exist
-	// if (linkedListLength < k || k < 1) {
-	// 	return;
-	// }
-
-	// let removedPosition = linkedListLength - k + 1;
-	// let currentPosition = 1;
-	// let removedNode = head;
-	// let prevOfRemovedNode = null;
-	// while (removedPosition !== currentPosition && node !== null) {
-	// 	currentPosition++;
-	// 	prevOfRemovedNode = removedNode;
-	// 	removedNode = removedNode.next;
-	// }
-
-	// prevOfRemovedNode.next = removedNode.next;
-
-	// console.log(linkedListLength);
 }
 
 // Do not edit the lines below.
