ebc91a4f59c4e9d15bf25d3441e9919fd4dc4273
Yu-Chun Tung <dyj0183@byui.edu>
2022-05-27 23:59:31 -0600

    tried to clean up

--- Coding Challenge Solutions/LinkedLists/sum-of-linked-lists.js
+++ Coding Challenge Solutions/LinkedLists/sum-of-linked-lists.js
@@ -28,12 +28,12 @@ function sumOfLinkedLists(linkedListOne, linkedListTwo) {
 
 	let newLinkedList = new LinkedList(newTotal[newTotal.length - 1]);
 	for (let i = newTotal.length - 2; i > -1; i--) {
-		newLinkedList.next.value = newTotal[i];
+		// newLinkedList.next.value = newTotal[i];
 		newLinkedList.next = newTotal[i];
 		newLinkedList = newLinkedList.next;
 	}
 
-	console.log(newLinkedList);
+	// console.log(newLinkedList);
 
 	return linkedListOne;
 }
