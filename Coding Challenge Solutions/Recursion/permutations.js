e844cc8a15d456d63d14e39b447120c64464022a
Yu-Chun Tung <dyj0183@byui.edu>
2022-06-02 23:38:35 -0600

    continued to work on the challenge of permutations

--- Coding Challenge Solutions/Recursion/permutations.js
+++ Coding Challenge Solutions/Recursion/permutations.js
@@ -1,4 +1,16 @@
-function getPermutations(array) {}
+// Sample input: array = [1, 2, 3]
+// Sample output: [
+//   [1, 2, 3],
+//   [1, 3, 2],
+//   [2, 1, 3],
+//   [2, 3, 1],
+//   [3, 1, 2],
+//   [3, 2, 1]
+// ]
+
+function getPermutations(array) {
+    
+}
 
 // Do not edit the line below.
 exports.getPermutations = getPermutations;
