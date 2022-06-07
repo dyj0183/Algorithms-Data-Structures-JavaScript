19146e28981e558a6bbc7440365923d8d7d70243
Yu-Chun Tung <dyj0183@byui.edu>
2022-06-06 22:16:54 -0600

    completed coding challenge of permutations

--- Coding Challenge Solutions/Recursion/permutations.js
+++ Coding Challenge Solutions/Recursion/permutations.js
@@ -8,8 +8,23 @@
 //   [3, 2, 1]
 // ]
 
+// This one is harder for me, come back and review again
 function getPermutations(array) {
-    
+	const permutations = [];
+	permutationsHelper(array, [], permutations);
+	return permutations;
+}
+
+function permutationsHelper(array, currentPermutation, permutations) {
+	if (!array.length && currentPermutation.length) {
+		permutations.push(currentPermutation);
+	} else {
+		for (let i = 0; i < array.length; i++) {
+			const newArray = array.slice(0, i).concat(array.slice(i + 1));
+			const newPermutation = currentPermutation.concat([array[i]]);
+			permutationsHelper(newArray, newPermutation, permutations);
+		}
+	}
 }
 
 // Do not edit the line below.
